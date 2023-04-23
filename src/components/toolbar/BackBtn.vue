e7fcee3ac36d546f6889c6095b2c22f330376b49
xiaoyan <icxiaoyanz@gmail.com>
2023-04-23 16:20:34 +0800

    :rocket: Reduce SVG size and optimize toggle-theme button

--- src/components/toolbar/BackBtn.vue
+++ src/components/toolbar/BackBtn.vue
@@ -5,42 +5,35 @@ const goBack = () => window.history.back()
 <template>
   <button class="btn-back" @click="goBack">
     <svg
-      width="24"
-      height="24"
+      width="28"
+      height="28"
       viewBox="0 0 48 48"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
     >
       <path
-        d="M12 23.9917H36"
+        d="M12 23.992h24M24 36L12 24l12-12"
         stroke="currentColor"
         stroke-width="4"
         stroke-linecap="round"
         stroke-linejoin="round"
-      ></path>
-      <path
-        d="M24 36L12 24L24 12"
-        stroke="currentColor"
-        stroke-width="4"
-        stroke-linecap="round"
-        stroke-linejoin="round"
-      ></path>
+      />
     </svg>
   </button>
 </template>
 
 <style lang="scss">
-  .btn-back {
-    width: 40px;
-    height: 40px;
-    padding: 8px;
-    border: none;
-    border-radius: 50%;
-    color: currentColor;
-    background-color: transparent;
+.btn-back {
+  width: 40px;
+  height: 40px;
+  padding: 6px;
+  border: none;
+  border-radius: 50%;
+  color: currentColor;
+  background-color: transparent;
 
-    &:hover {
-      background-color: var(--hover-color);
-    }
+  &:hover {
+    background-color: var(--hover-color);
   }
+}
 </style>
