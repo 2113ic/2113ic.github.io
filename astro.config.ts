9ba3d864f94b4fe58ca34018bc4db65b92a09629
2113ic <icxiaoyanz@gmail.com>
2023-09-16 21:14:40 +0800

    refactor(toolbar): vue -> astro

--- astro.config.ts
+++ astro.config.ts
@@ -1,19 +1,11 @@
 import { defineConfig } from 'astro/config'
-import vue from '@astrojs/vue'
-import remarkToc from 'remark-toc'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [vue()],
   site: 'https://2113ic.github.io/',
   markdown: {
-    remarkPlugins: [
-      remarkToc.bind(this, {
-        heading: '目录',
-      })
-    ],
     shikiConfig: {
-      theme: 'dracula',
+      theme: 'min-dark',
     },
   },
 })
