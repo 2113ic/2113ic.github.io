3b666d7687b87aaf9167cc78ed371e783c53498c
2113ic <icxiaoyanz@gmail.com>
2023-09-17 23:42:01 +0800

    feat: astro to v3 and code copy

--- astro.config.ts
+++ astro.config.ts
@@ -1,11 +1,13 @@
-import { defineConfig } from 'astro/config'
+import { defineConfig } from 'astro/config';
+import expressiveCode from "astro-expressive-code";
 
 // https://astro.build/config
 export default defineConfig({
+  integrations: [expressiveCode()],
   site: 'https://2113ic.github.io/',
   markdown: {
     shikiConfig: {
-      theme: 'min-dark',
-    },
-  },
-})
+      theme: 'min-dark'
+    }
+  }
+});
